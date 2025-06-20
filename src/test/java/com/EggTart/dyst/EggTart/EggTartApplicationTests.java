88c75dac48612a8c1a03f87c2718b22e68796452
Shawn Dong <yanshengtian@gmail.com>
2025-06-21 00:14:32 +0800

    test: Add basic test structure - Add EggTartApplicationTests for context loading - Set up test infrastructure for future test development - Include basic Spring Boot test configuration

---
+++ src/test/java/com/EggTart/dyst/EggTart/EggTartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EggTart.dyst.EggTart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggTartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
